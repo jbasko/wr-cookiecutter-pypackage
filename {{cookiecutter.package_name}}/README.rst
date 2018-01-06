a3fb67f92d2d13f6706d6425992c0f21db45a352
Jazeps Basko <jazeps.basko@gmail.com>
2018-01-06 13:42:31 +0000

    Initial commit

---
+++ {{cookiecutter.package_name}}/README.rst
@@ -0,0 +1,7 @@
+*****************************
+{{cookiecutter.package_name}}
+*****************************
+
+.. code-block:: shell
+
+    pip install {{cookiecutter.package_name}}
---
+++ {{cookiecutter.package_name}}/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='{{cookiecutter.package_name}}',
+    version=read('{{cookiecutter.package_directory}}/__init__.py').split('\n')[0].split('=', 1)[1].strip().strip("'"),
+    author='{{cookiecutter.package_author}}',
+    author_email='{{cookiecutter.package_email}}',
+    maintainer='{{cookiecutter.package_author}}',
+    maintainer_email='{{cookiecutter.package_email}}',
+    license='MIT',
+    url='{{cookiecutter.package_url}}',
+    description='{{cookiecutter.package_name}}',
+    long_description=read('README.rst'),
+    packages=['{{cookiecutter.package_directory}}'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
